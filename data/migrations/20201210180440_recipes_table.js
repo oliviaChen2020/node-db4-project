f284f29462702c6fc8605f4838bc8cb3178d8707
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-12-10 18:26:50 -0500

    tables are ready to use for getting data from the endpoints

---
+++ data/migrations/20201210180440_recipes_table.js
@@ -0,0 +1,157 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('recipes', (table) => {
+      table.increments('recipe_id');
+      table.string('recipe_name', 128).notNullable();
+    })
+    .createTable('ingredients', (table) => {
+      table.increments('ingredient_id');
+      table.string('ingredient_name', 128).notNullable();
+    })
+    .createTable('steps', (table) => {
+      table.increments('step_id');
+      table.string('step_number', 128).notNullable();
+      table.string('step_description', 128).notNullable();
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+    })
+    .createTable('recipe_ingredients', (table) => {
+      table.increments('recipe_ingredients_id');
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      table
+        .integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      table.string('amount', 128).notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
+};
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('recipes', (table) => {
+      table.increments('recipe_id');
+      table.string('recipe_name', 128).notNullable();
+    })
+    .createTable('ingredients', (table) => {
+      table.increments('ingredient_id');
+      table.string('ingredient_name', 128).notNullable();
+    })
+    .createTable('steps', (table) => {
+      table.increments('step_id');
+      table.string('step_number', 128).notNullable();
+      table.string('step_description', 128).notNullable();
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+    })
+    .createTable('recipe_ingredients', (table) => {
+      exports.up = function (knex) {
+        return knex.schema
+          .createTable('recipes', (table) => {
+            table.increments('recipe_id');
+            table.string('recipe_name', 128).notNullable();
+          })
+          .createTable('ingredients', (table) => {
+            table.increments('ingredient_id');
+            table.string('ingredient_name', 128).notNullable();
+          })
+          .createTable('steps', (table) => {
+            table.increments('step_id');
+            table.string('step_number', 128).notNullable();
+            table.string('step_description', 128).notNullable();
+            table
+              .integer('recipe_id')
+              .unsigned()
+              .notNullable()
+              .references('recipe_id')
+              .inTable('recipes')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+          })
+          .createTable('recipe_ingredients', (table) => {
+            table.increments('recipe_ingredients_id');
+            table
+              .integer('recipe_id')
+              .unsigned()
+              .notNullable()
+              .references('recipe_id')
+              .inTable('recipes')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+            table
+              .integer('ingredient_id')
+              .unsigned()
+              .notNullable()
+              .references('ingredient_id')
+              .inTable('ingredients')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+            table.string('amount', 128).notNullable();
+          });
+      };
+
+      exports.down = function (knex) {
+        return knex.schema
+          .dropTableIfExists('recipe_ingredients')
+          .dropTableIfExists('steps')
+          .dropTableIfExists('ingredients')
+          .dropTableIfExists('recipes');
+      };
+
+      table.increments('recipe_ingredients_id');
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      table
+        .integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT');
+      table.string('amount', 128).notNullable();
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('recipe_ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
+};
